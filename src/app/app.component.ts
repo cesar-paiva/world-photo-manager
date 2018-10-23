a7a55ca801bb2f004a442ad157190f6aacd53023
cesar-paiva <cesar.giupponi@gmail.com>
2018-10-22 23:04:13 -0300

    Creating the first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  photos = [
+    {
+      url: 'https://cdn.motor1.com/images/mgl/0beAj/s1/2019-bmw-m2-competition.jpg',
+      description: 'BMW Branca'
+    },
+    {
+      url: 'https://abrilquatrorodas.files.wordpress.com/2018/03/qr-706-car-bmw-x2-0099.jpg?quality=70&strip=info&strip=info',
+      description: 'BMW Amarela'
+    }
+  ];
 }
